de686d803ef2b4f9ceec2a41c7c0ce27cf4f6ed6
TechnologyTim <timwalter23@protonmail.com>
2024-02-06 09:57:59 +0100

    added personal nix flake

---
+++ os_config/nixos/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Tims personal flake";
+
+  inputs = {
+	system = "x86_64-linux";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+	home-manager.url = "github:nix-community/home-manager/release-23.11";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+	let
+	  lib = nixpkgs.lib;
+	  system = "x86_64-linux";
+	  pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		nixosConfigurations = {
+		  nixos = lib.nixosSystem {
+		    inherit system;
+		    modules = [./configuration.nix];
+		  };
+	    };
+		homeConfigurations = {
+		  tim = home-manager.lib.homeManagerConfiguration {
+         	inherit pkgs;
+        	modules = [./home.nix];
+      	};
+	  };
+   };
+}
